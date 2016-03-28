80cd3ba8effcde5d2a1c3b6129ef760d8382a50e
Ryukie <ryukie.sama@gmail.com>
2016-03-28 23:38:03 +0800

    init绘图区域

--- RyuGradientPolyline/RyuGradientPolyline/ViewController.m
+++ RyuGradientPolyline/RyuGradientPolyline/ViewController.m
@@ -14,7 +14,7 @@
 @interface ViewController ()<MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mv_mapView;
 @property (nonatomic,strong) CLLocationManager *locateManager;
-
+@property (nonatomic,strong) MKPolygon *polygon;//绘制多边形
 @end
 
 @implementation ViewController
@@ -51,6 +51,36 @@
      MKMapTypeHybridFlyover NS_ENUM_AVAILABLE(10_11, 9_0),
      */
     self.mv_mapView.mapType = MKMapTypeStandard;
+}
+- (IBAction)drawPolygon:(id)sender {
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(23.29043, 112.81630), 500, 500);
+    [_mv_mapView setRegion:region];
+    
+    
+    CLLocationCoordinate2D points[4];//NSArry 只能存放OC对象   这里用MKPoint就可以用了
+    CLLocationCoordinate2D lt = CLLocationCoordinate2DMake(23.29131,112.8155);
+    CLLocationCoordinate2D rt = CLLocationCoordinate2DMake(23.29061, 112.8177);
+    CLLocationCoordinate2D lb = CLLocationCoordinate2DMake(23.29014, 112.8151);
+    CLLocationCoordinate2D rb = CLLocationCoordinate2DMake(23.28949, 112.8173);
+    points[0] = lt;
+    points[1] = rt;
+    points[2] = rb;
+    points[3] = lb;//需要注意顺序
+    
+    self.polygon = [MKPolygon polygonWithCoordinates:points count:4];
+    //    [MKPolygon polygonWithPoints:<#(nonnull MKMapPoint *)#> count:<#(NSUInteger)#>]
+    NSLog(@"%@",self.polygon);
+    [self.mv_mapView addOverlay:_polygon level:MKOverlayLevelAboveLabels];
+    //MKOverlayLevelAboveRoads = 0, // note that labels include shields and point of interest icons.
+    //    MKOverlayLevelAboveLabels
+}
+#pragma mark - Ryukie:MapView 绘制蒙版的代理
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    UIColor* purpleColor = [UIColor colorWithRed:0.149f green:0.0f blue:0.40f alpha:0.5f];
+    MKPolygonRenderer *polygonRenderer = [[MKPolygonRenderer alloc] initWithOverlay:overlay];
+    polygonRenderer.fillColor = purpleColor;
+    return polygonRenderer;
 }
 
 @end
